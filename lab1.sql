a19978ac9483977d4bb4d9a75beb272307701e0a
kkamochka <157725905+kkamochka@users.noreply.github.com>
2024-09-17 18:41:16 +0500

    Add files via upload

---
+++ lab1.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE lab 1;
+USE "lab 1";
+CREATE TABLE users (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    PRIMARY KEY (id)
+);
+ALTER TABLE users ADD isadmin INT;
+ALTER TABLE users MODIFY isadmin BOOLEAN;
+ALTER TABLE users ALTER COLUMN isadmin SET DEFAULT FALSE;
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(50),
+    user_id INT,
+    PRIMARY KEY (id)
+);
+DROP TABLE tasks;
+DROP DATABASE "lab 1";
