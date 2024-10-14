7232e2bc513125cbeed4607216fa355b47c27798
kkamochka <157725905+kkamochka@users.noreply.github.com>
2024-10-14 17:20:32 +0500

    Update lab5.sql

--- lab5.sql
+++ lab5.sql
@@ -1,29 +1,37 @@
+DROP database lab5;
 CREATE DATABASE lab5;
 
-CREATE TABLE Customers (
-    customer_id INT,
-    customer_name VARCHAR(255),
-    city VARCHAR(255),
-    grade INT,
-    salesman_id INT
+drop table customers;
+
+CREATE TABLE customers(
+    customer_id INT PRIMARY KEY,
+    cust_name VARCHAR(255) ,
+    city VARCHAR(255) ,
+    grade integer,
+    salesman_id VARCHAR(255)
 );
 
-CREATE TABLE Orders (
-    order_number INT,
-    purchase_amount DECIMAL(10, 2),
+drop table orders;
+
+CREATE TABLE orders(
+    order_num INT PRIMARY KEY,
+    puch_amount DECIMAL(10,2),
     order_date DATE,
     customer_id INT,
-    salesman_id INT
-);
-
-CREATE TABLE Salesman (
     salesman_id INT,
+    name VARCHAR(255),
+    city VARCHAR(255),
+    comission INT
+    );
+
+CREATE TABLE salesman (
+    salesman_id INT PRIMARY KEY,
     name VARCHAR(255),
     city VARCHAR(255),
     commission DECIMAl(3, 2)
 );
 
-INSERT INTO Customers VALUES
+INSERT INTO customers VALUES
 (3002, 'Nick Rimando', 'New York', 100, 5001),
 (3005, 'Graham Zusi', 'California', 200, 5002),
 (3001, 'Brad Guzan', 'London', 100, 5005),
@@ -32,7 +40,7 @@ INSERT INTO Customers VALUES
 (3009, 'Geoff Camero', 'Berlin', 100, 5003),
 (3008, 'Julian Green', 'London', 300, 5002);
 
-INSERT INTO Orders VALUES
+INSERT INTO orders VALUES
 (70001, 150.50, '2012-10-05', 3005, 5002),
 (70009, 270.65, '2012-09-10', 3001, 5005),
 (70002, 65.26, '2012-10-05', 3002, 5001),
@@ -41,7 +49,7 @@ INSERT INTO Orders VALUES
 (70005, 2400.60, '2012-07-27', 3007, 5001),
 (70008, 5760.00, '2012-09-10', 3002, 5001);
 
-INSERT INTO Salesman VALUES
+INSERT INTO salesman VALUES
 (5001, 'James Hoog', 'New York', 0.15),
 (5002, 'Nail Knite', 'Paris', 0.13),
 (5005, 'Pit Alex', 'London', 0.11),
@@ -49,22 +57,29 @@ INSERT INTO Salesman VALUES
 (5003, 'Lauson Hen', 'Berlin', 0.12),
 (5007, 'Paul Adam', 'Rome', 0.13);
 
---3 задание
-SELECT SUM(purchase_amount) AS total_purchase_amount
-FROM Orders;
+/* 3. Select the total purchase amount of all orders.*/
+SELECT SUM(puch_amount) AS total_purchase_amount
+FROM orders;
 
---4 задание
-SELECT AVG(purchase_amount) AS average_purchase_amount
-FROM Orders;
+/* 4. Select the average purchase amount of all orders.*/
+SELECT AVG(puch_amount) AS total_purchase_amount
+FROM orders;
 
---5 задание
-SELECT COUNT(customer_name) AS count_customer_name
-FROM Customers;
+/*5. Select how many customer have listed their names.*/
+SELECT COUNT(DISTINCT cust_name) AS number_of_customers
+FROM customers;
 
---6 задание
-SELECT MIN(purchase_amount) AS minimum_purchase_amount
-FROM Orders;
+/* 6. Select the minimum purchase amount of all the orders.*/
+SELECT MIN(puch_amount) AS minimum_purchase_amount
+FROM orders;
 
---7 задание
-SELECT * FROM customers
-WHERE customer_name LIKE '%b';
+/* 7. Select customer with all information whose name ends with the
+letter 'b'.*/
+SELECT *
+FROM customers
+WHERE cust_name LIKE '%b';
+
+/* 8. Select the average purchase amount of all orders made in the
+last 30 days.*/
+SELECT AVG(puch_amount) AS average_purchase_amount
+FROM orders
